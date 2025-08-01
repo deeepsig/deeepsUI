883b827fc06feafe92e8765e7ea36b3fc3e463db
deeepsig <deepakgdkg1g8868@gmail.com>
2025-08-01 23:11:22 +0530

    added wip component

--- src/components/chatBubble/SecondaryBubble.tsx
+++ src/components/chatBubble/SecondaryBubble.tsx
@@ -4,12 +4,12 @@ interface SecondaryBubbleProps {
 }
 
 export default function SecondaryBubble({
-  text = 'Code the UI of a chat bubble',
+  text = 'find me some good brand design inspirations',
   isEditing = false,
 }: SecondaryBubbleProps) {
   return (
     <div
-      className={`inline-block py-2 px-4 rounded-2xl ${!isEditing ? 'bg-[#F4F4F4] border border-[#f1f1f1]' : ''}`}
+      className={`inline-block py-2 px-4 rounded-2xl ${!isEditing ? 'bg-[#FaFcFe] border border-[#f1f1f1]' : ''}`}
     >
       <p className="font-inter text-[#2a2c2e]">{text}</p>
     </div>
