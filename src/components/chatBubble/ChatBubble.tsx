3574155c44c47a5347d16830034942393e6c8abb
deeepsig <deepakgdkg1g8868@gmail.com>
2025-08-01 16:34:00 +0530

    minor styling and html element replacement

--- src/components/chatBubble/ChatBubble.tsx
+++ src/components/chatBubble/ChatBubble.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
-
 interface ChatBubbleProps {
   text?: string;
   isEditing?: boolean;
 }
 
-const ChatBubble: React.FC<ChatBubbleProps> = ({
+export default function ChatBubble({
   text = 'Code the UI of a chat bubble',
   isEditing = false,
-}) => {
+}: ChatBubbleProps) {
   return (
     <div
       className={`inline-block py-2 px-4 rounded-2xl ${!isEditing ? 'bg-[#F4F4F4] border border-[#f1f1f1]' : ''}`}
     >
-      <code className="text-base font-inter text-[#2a2c2e]">{text}</code>
+      <p className="text-base font-inter text-[#2a2c2e]">{text}</p>
     </div>
   );
-};
-
-export default ChatBubble;
+}
